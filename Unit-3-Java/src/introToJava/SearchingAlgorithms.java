39751ed59c1b39573c8abaa184856c98126e0e63
Nathaniel Dizon <132396054+HTTime39@users.noreply.github.com>
2023-05-31 11:05:13 -0700

    Searching algorithm lesson.

---
+++ Unit-3-Java/src/introToJava/SearchingAlgorithms.java
@@ -0,0 +1,59 @@
+package introToJava;
+
+public class SearchingAlgorithms {
+
+	public static void main(String[] args) {
+		//===== Searching Alogrithms =====//
+		
+		//Linear Search: Checks an array's value from start to finish or until it finds the value that it is searching for.
+		
+		int [] values = {0, 1, 2, 3, 4, 5, 6};
+		
+		System.out.println(linearSearch(values, 3));
+		
+		//Binary Search: Cuts a data set in half until it finds a desired value. It only works when data is stored in order of least to greatest. 
+		
+		System.out.println(binarySearch(values, 5));
+
+	}
+
+	public static boolean linearSearch(int [] values, int desiredValue)
+	{
+		for (int i = 0; i < values.length; i++)
+		{
+			if (values[i] == desiredValue)
+			{
+				return(true);
+			}
+		}
+		return(false);
+	}
+	
+	public static boolean binarySearch(int[] values, int desiredValue)
+	{
+		int low = 0;
+		int high = values.length - 1;
+		
+		while (low <= high)
+		//There are numbers to search, as long as the low and the high are not the same.
+		{
+			int middle = (low + high) / 2;
+			int currentValue = values[middle];
+			//The value of the index at the center of the current bounds within the dataset.
+			
+			if (currentValue == desiredValue)
+			{
+				return true;
+			}
+			else if (currentValue > desiredValue)
+			{
+				high = currentValue;
+			}
+			else if (currentValue < desiredValue)
+			{
+				low = currentValue;
+			}
+		}
+		return(false);
+	}
+}
