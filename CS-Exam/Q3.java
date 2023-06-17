9a34f34030a03f56271104bf934c8f863fbf455a
HTTime39 <132396054+HTTime39@users.noreply.github.com>
2023-06-16 19:03:01 -0700

    Exam Complete

---
+++ CS-Exam/Q3.java
@@ -0,0 +1,16 @@
+package q3;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] favouriteMovies = {"Wall-E", "Bullet Train", "Finding Nemo", "Toy Story", "The Avengers"};
+		
+		for (int i = 0; i < favouriteMovies.length; i++)
+		{
+			System.out.println(favouriteMovies[i]);
+		}
+		
+	}
+
+}
---
+++ CS-Exam/q2.js
@@ -0,0 +1,10 @@
+let person =
+{
+    name: "Nathaniel",
+    age: 18,
+    school: "Sacred Heart",
+
+    activities: ["Eating Food", "Video Games", "YouTube"]
+}
+
+console.log("My name is " + person.name + " and I am " + person.age + " years old. I attend " + person.school + " and I some activities that I like are " + person.activities[0] + ", " + person.activities[1] + ", and " + person.activities[2]);
